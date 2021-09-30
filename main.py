941a85e506bc23c6ab322905e6ef773dcef2778f
root <Alexandre Falardeau>
2021-09-30 19:28:36 +0000

    Add get request from bored api

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
 from requests import get
 
+# Free APIs to explore
+
+BORED_API_URL = "https://www.boredapi.com/api"
+
+
 def get_some_data():
-    res = get("https://api.publicapis.org/entries")
-    print(res.status)
-    print(res.data)
+    res = get(f"{BORED_API_URL}/activity")
+    print(res)
+    print(res.status_code)
+    print(res.json())
 
 if __name__ == "__main__":
+    get_some_data()
     print("something")
